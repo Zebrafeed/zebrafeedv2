de57654ccbbbc4a9a45efea9f703fa75b3f8d200
John Eriksson <yanis.cool3@gmail.com>
2013-10-14 00:03:47 +0200

    Made more responsive
    Changes depending on width on the fly

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,21 @@
 var pages;
+var content;
 
 $(document).ready(function(){
+	content = $('#content');
 	pages = [{img: 'img/bg0.jpg', data: 'data.html #home'}, {img: 'img/bg7.jpg', data: 'data.html #portfolio'}, {img: 'img/bg8.jpg', data: 'data.html #pricing'}, {img: 'img/bg3.jpg', data: 'data.html #contact'}, {img: 'img/bg7.jpg', data: ''}];
-	console.log(pages);
-	if($(window).width() > 1200){
-		$('#nav').css('margin-top', $(window).height()-90);
-		$('#content').load(pages[0].data, function(){
-			$('#content').fadeIn(500);
-		});
-	}else{
-		$('#mobileC10').load("data.html #home");
-		$('#mobileC20').load("data.html #portfolio");
-		$('#mobileC30').load("data.html #pricing");
-	}
+	
+	displayMobile();
+
+	$(window).resize(function(){
+		displayMobile();
+	});
 });
 
 
 var changeBg = function(bgNr){
-	$('#content').hide();
-	$('#content').html("");
+	content.hide();
+	content.html("");
 
 	$('body').css({background: 'url("'+pages[bgNr].img+'") no-repeat center center fixed'});
 	$('body').css({'-webkit-background-size': 'cover'});
@@ -26,8 +23,8 @@ var changeBg = function(bgNr){
 	$('body').css({'-o-background-size': 'cover'});
 	$('body').css({'background-size': 'cover'});
 
-	$('#content').load(pages[bgNr].data, function(){
-		$('#content').fadeIn(500);
+	content.load(pages[bgNr].data, function(){
+		content.fadeIn(500);
 	});
 }
 
@@ -38,5 +35,20 @@ var bubble = function(el){
 		el.children('div').slideUp("fast");
 	}else{
 		el.children('div').slideDown("fast");
+	}
+}
+
+var displayMobile = function(){
+	if($(window).width() > 800){
+		$('#nav').css('margin-top', $(window).height()-90);
+		content.load(pages[0].data, function(){
+			content.fadeIn(500);
+		});
+	}else{
+		content.hide();
+		$('#nav').css('margin-top', 0);
+		$('#mobileC10').load("data.html #home");
+		$('#mobileC20').load("data.html #portfolio");
+		$('#mobileC30').load("data.html #pricing");
 	}
 }
