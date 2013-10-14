ca59ff911cceeef5918529417844247674c5f2ff
John Eriksson <yanis.cool3@gmail.com>
2013-10-14 03:37:07 +0200

    BIG ui overhaul
    Dynamic backgrounds
    Content slides
    Tweaked responsiveness

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,24 @@
 var pages;
 var content;
+var bgImg;
 
 $(document).ready(function(){
 	content = $('#content');
-	pages = [{img: 'img/bg0.jpg', data: 'data.html #home'}, {img: 'img/bg7.jpg', data: 'data.html #portfolio'}, {img: 'img/bg8.jpg', data: 'data.html #pricing'}, {img: 'img/bg3.jpg', data: 'data.html #contact'}, {img: 'img/bg7.jpg', data: ''}];
+	bgImg = $('#bgImg');
+	pages = [{img: 'img/bg0.jpg', data: 'data.html #home'}, {img: 'img/bg1.jpg', data: 'data.html #portfolio'}, {img: 'img/bg2.jpg', data: 'data.html #pricing'}, {img: 'img/bg3.jpg', data: 'data.html #contact'}];
 	
-	displayMobile();
+	if($(window).width() > 800){
+		content.load(pages[0].data, function(){
+			content.fadeIn(500);
+		});
+	}else{
+		content.hide();
+		$('#nav').css('margin-top', 0);
+		$('#mobileC10').load("data.html #home");
+		$('#mobileC20').load("data.html #portfolio");
+		$('#mobileC30').load("data.html #pricing");
+	}
+	
 
 	$(window).resize(function(){
 		displayMobile();
@@ -17,15 +30,38 @@ var changeBg = function(bgNr){
 	content.hide();
 	content.html("");
 
-	$('body').css({background: 'url("'+pages[bgNr].img+'") no-repeat center center fixed'});
-	$('body').css({'-webkit-background-size': 'cover'});
-	$('body').css({'-moz-background-size': 'cover'});
-	$('body').css({'-o-background-size': 'cover'});
-	$('body').css({'background-size': 'cover'});
-
-	content.load(pages[bgNr].data, function(){
-		content.fadeIn(500);
+	change(pages[bgNr].img, function(){
+		content.load(pages[bgNr].data, function(){
+			content.show();
+			content.animate({
+				'right': '0px'
+			}, 500);
+		});
 	});
+}
+
+var change = function(path, callback){
+	var bg = $('#bg');
+	var bg2 = $('#bg2');
+
+	content.css('right', '-50%');
+
+	bg2.css({
+		'background': 'url("'+path+'") no-repeat center center fixed',
+		'background-size': 'cover'
+	});
+	bg.animate({
+		'opacity': '0'
+	}, 500, function(){
+		bg.css({
+			'background': 'url("'+path+'") no-repeat center center fixed',
+			'background-size': 'cover',
+			'opacity': '1'
+		});
+	});
+	setTimeout(function(){
+		callback();
+	}, 500);
 }
 
 var bubble = function(el){
@@ -40,10 +76,7 @@ var bubble = function(el){
 
 var displayMobile = function(){
 	if($(window).width() > 800){
-		$('#nav').css('margin-top', $(window).height()-90);
-		content.load(pages[0].data, function(){
-			content.fadeIn(500);
-		});
+		
 	}else{
 		content.hide();
 		$('#nav').css('margin-top', 0);
